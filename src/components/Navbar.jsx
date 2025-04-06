e74c2497888c46a9307e652741f20eaf45b5ec70
Siddharth Potphode <siddharthpotphode7@gmail.com>
2025-04-06 12:17:39 +0530

    fixing issues

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         <div className="flex items-center space-x-4">
           <div className="w-10 h-10 bg-gray-700 rounded-full">
             <img
-              src="https://github.com/SiddharthP121/siddhartha-portfolio/blob/main/public/assets/logo.jpg?raw=true"
+              src="src\assets\WhatsApp Image 2025-04-05 at 17.00.26_3a71b0b0.jpg"
               alt=""
               className="rounded-full"
             />
