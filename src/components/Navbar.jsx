2ee8cbd9f69cebc715cd1888d107033e24c0c30b
Siddharth Potphode <siddharthpotphode7@gmail.com>
2025-04-08 00:35:57 +0530

    All done for today

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,15 @@ import { MdEmail } from "react-icons/md";
 import { BsTwitterX } from "react-icons/bs";
 import { IoLogoWhatsapp } from "react-icons/io";
 import { FaInstagram } from "react-icons/fa";
-
-
-
+import { MdOutlinePhoneAndroid } from "react-icons/md";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [socialCon, setCocialCon] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+    setCocialCon(!socialCon);
   };
 
   return (
@@ -28,7 +28,9 @@ const Navbar = () => {
               className="rounded-full"
             />
           </div>
-          <div className="text-lg font-bold">{" <Siddharth /> "}</div>
+          <div id="name" className="text-2xl font-bold">
+            {" <Siddharth /> "}
+          </div>
         </div>
 
         {/* Center Section: Navigation Links */}
@@ -56,10 +58,7 @@ const Navbar = () => {
         </ul>
 
         {/* Menu Button */}
-        <button
-          onClick={toggleMenu}
-          className="text-black focus:outline-none"
-        >
+        <button onClick={toggleMenu} className="text-black focus:outline-none">
           <lord-icon
             src="https://cdn.lordicon.com/jectmwqf.json"
             trigger="hover"
@@ -67,6 +66,87 @@ const Navbar = () => {
             style={{ width: "30px", height: "30px" }}
           ></lord-icon>
         </button>
+
+        {socialCon && (
+          <div className="absolute top-16 right-6 bg-white shadow-md rounded-md p-4 z-20 ">
+            <div className="socials flex flex-col space-y-4">
+              <h3 className="font-bold">Connect with me</h3>
+              <ul className="space-y-2">
+                <li className="flex items-center space-x-2">
+                  <span className="logo-social">
+                    <IoLogoLinkedin />
+                  </span>
+                  <a
+                    href="https://www.linkedin.com/in/siddhartha-potphode"
+                    className="hover:text-purple-500"
+                  >
+                    LinkedIn
+                  </a>
+                </li>
+                <li className="flex items-center space-x-2">
+                  <span className="logo-social">
+                    <ImGithub />
+                  </span>
+                  <a href="#" className="hover:text-purple-500">
+                    GitHub
+                  </a>
+                </li>
+                <li className="flex items-center space-x-2">
+                  <span className="logo-social">
+                    <MdEmail />
+                  </span>
+                  <a
+                    href="mailto:siddharthpotphode7@gmail.com"
+                    className="hover:text-purple-500"
+                  >
+                    E-mail
+                  </a>
+                </li>
+                <li className="flex items-center space-x-2">
+                  <span className="logo-social">
+                    <BsTwitterX />
+                  </span>
+                  <a
+                    href="https://x.com/siddharth_wd121"
+                    className="hover:text-purple-500"
+                  >
+                    Twitter
+                  </a>
+                </li>
+                <li className="flex items-center space-x-2">
+                  <span className="logo-social">
+                    <MdOutlinePhoneAndroid />
+                  </span>
+                  <a href="tel:+918109013328" className="hover:text-purple-500">
+                    Call me
+                  </a>
+                </li>
+                <li className="flex items-center space-x-2">
+                  <span className="logo-social">
+                    <IoLogoWhatsapp />
+                  </span>
+                  <a
+                    href="https://wa.me/+918109013328"
+                    className="hover:text-purple-500"
+                  >
+                    WhatsApp
+                  </a>
+                </li>
+                <li className="flex items-center space-x-2">
+                  <span className="logo-social">
+                    <FaInstagram />
+                  </span>
+                  <a
+                    href="https://instagram.com/siddharth_wd"
+                    className="hover:text-purple-500"
+                  >
+                    Instagram
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
 
         {/* Dropdown Menu */}
         {isMenuOpen && (
@@ -95,48 +175,84 @@ const Navbar = () => {
             </ul>
             <div className="partition h-0.5 bg-gray-500 w-[37vw] my-3"></div>
             <div className="socials flex flex-col space-y-4">
-  <h3 className="font-bold">Connect with me</h3>
-  <ul className="space-y-2">
-    <li className="flex items-center space-x-2">
-      <span className="logo-social">
-        <IoLogoLinkedin />
-      </span>
-      <a href="#" className="hover:text-purple-500">LinkedIn</a>
-    </li>
-    <li className="flex items-center space-x-2">
-      <span className="logo-social">
-        <ImGithub />
-      </span>
-      <a href="#" className="hover:text-purple-500">GitHub</a>
-    </li>
-    <li className="flex items-center space-x-2">
-      <span className="logo-social">
-        <MdEmail />
-      </span>
-      <a href="#" className="hover:text-purple-500">E-mail</a>
-    </li>
-    <li className="flex items-center space-x-2">
-      <span className="logo-social">
-        <BsTwitterX />
-      </span>
-      <a href="#" className="hover:text-purple-500">Twitter</a>
-    </li>
-    <li className="flex items-center space-x-2">
-      <span className="logo-social">
-      <IoLogoWhatsapp />
-
-      </span>
-      <a href="#" className="hover:text-purple-500">WhatsApp</a>
-    </li>
-    <li className="flex items-center space-x-2">
-      <span className="logo-social">
-      <FaInstagram />
-
-      </span>
-      <a href="#" className="hover:text-purple-500">Instagram</a>
-    </li>
-  </ul>
-</div>
+              <h3 className="font-bold">Connect with me</h3>
+              <ul className="space-y-2">
+                <li className="flex items-center space-x-2">
+                  <span className="logo-social">
+                    <IoLogoLinkedin />
+                  </span>
+                  <a
+                    href="https://www.linkedin.com/in/siddhartha-potphode"
+                    className="hover:text-purple-500"
+                  >
+                    LinkedIn
+                  </a>
+                </li>
+                <li className="flex items-center space-x-2">
+                  <span className="logo-social">
+                    <ImGithub />
+                  </span>
+                  <a
+                    href="https://github.com/SiddharthP121"
+                    className="hover:text-purple-500"
+                  >
+                    GitHub
+                  </a>
+                </li>
+                <li className="flex items-center space-x-2">
+                  <span className="logo-social">
+                    <MdEmail />
+                  </span>
+                  <a
+                    href="mailto:siddharthpotphode7@gmail.com"
+                    className="hover:text-purple-500"
+                  >
+                    E-mail
+                  </a>
+                </li>
+                <li className="flex items-center space-x-2">
+                  <span className="logo-social">
+                    <BsTwitterX />
+                  </span>
+                  <a
+                    href="https://x.com/siddharth_wd121"
+                    className="hover:text-purple-500"
+                  >
+                    Twitter
+                  </a>
+                </li>
+                <li className="flex items-center space-x-2">
+                  <span className="logo-social">
+                    <MdOutlinePhoneAndroid />
+                  </span>
+                  <a href="tel:+918109013328" className="hover:text-purple-500">
+                    Call me
+                  </a>
+                </li>
+                <li className="flex items-center space-x-2">
+                  <span className="logo-social">
+                    <IoLogoWhatsapp />
+                  </span>
+                  <a
+                    href="https://wa.me/+918109013328"
+                    className="hover:text-purple-500"
+                  >
+                    WhatsApp
+                  </a>
+                </li>
+                <li className="flex items-center space-x-2">
+                  <span className="logo-social">
+                    <FaInstagram />
+                  </span>
+                  <a
+                    href="https://instagram.com/siddharth_wd"
+                    className="hover:text-purple-500"
+                  >
+                    Instagram
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         )}
       </div>
