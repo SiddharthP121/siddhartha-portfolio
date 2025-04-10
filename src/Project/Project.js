0abf1383e3c038429b9ba65b53035e2461aa2e30
Siddharth Potphode <siddharthpotphode7@gmail.com>
2025-04-11 01:39:40 +0530

    All done for the project section

---
+++ src/Project/Project.js
@@ -0,0 +1,39 @@
+const projectsData = [
+    {
+      id: 1,
+      name: "Portfolio -- Siddharth Potphode",
+      img: "https://github.com/SiddharthP121/siddhartha-portfolio/blob/main/public/assets/protfolio.png?raw=true",
+      description:
+        "This page is deployed on GitHub Pages and uses ReactJS (for frontend), NodeJS, ExpressJS (for backend), and Tailwind CSS (for designs and responsiveness). The page depicts my overall progress, work, and skills that I have learned. Various animated icons and buttons are used to create a modern frontend. The code is updated on GitHub and is open for all.",
+    },
+    {
+      id: 2,
+      name: "We-Con -- Connect the People Worldwide",
+      img: "https://github.com/SiddharthP121/siddhartha-portfolio/blob/main/public/assets/We-con.png?raw=true",
+      description:
+        "We-Con is a social media platform prototype. It is a ReactJS-based project with Tailwind CSS implemented. The page has fully functional Login and Signup pages, allowing users to register and create accounts. The page is responsive and has a good UI.",
+    },
+    {
+      id: 3,
+      name: "Save-Pass -- Manage Your Passwords",
+      img: "https://github.com/SiddharthP121/siddhartha-portfolio/blob/main/public/assets/Passmanager.png?raw=true",
+      description:
+        "Save-Pass is a password manager that allows users to securely store and manage their passwords. It uses encryption to protect sensitive information and provides a user-friendly interface for easy access. The application is built using the MERN stack.",
+    },
+    {
+      id: 4,
+      name: "Netflix (Clone) -- Watch Movies Online",
+      img: "https://github.com/SiddharthP121/siddhartha-portfolio/blob/main/public/assets/netflix.png?raw=true",
+      description:
+        "This is a single web page designed to practice HTML and CSS skills. It is a clone of the Netflix page. I've learned various basic concepts of CSS and HTML. The page is responsive and has a good UI.",
+    },
+    {
+      id: 5,
+      name: "Laptoverse -- Buy Laptops Online",
+      img: "https://github.com/SiddharthP121/siddhartha-portfolio/blob/main/public/assets/laptoverse.png?raw=true",
+      description:
+        "This project is my college assignment, and I used HTML, CSS, and JavaScript as my main tech stack. The page is responsive and has a good UI. The code is updated on GitHub.",
+    },
+  ];
+  
+  export default projectsData;
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import projectsData from "../Project/Project";
 
 const Project = () => {
+  const handleDetails = (id) => {
+    console.log(`Project ID: ${id}`);
+  };
+
   return (
     <>
       <div className="project flex flex-col justify-center items-center">
@@ -11,120 +16,39 @@ const Project = () => {
           project showcases my skills and creativity in development. Feel free
           to explore them and reach out if you have any questions or feedback!
         </p>
-         {/* Project container: 1 */}
-        
-         <div className="my-10 sm:my-20 project-card flex flex-wrap justify-around items-start gap-8 w-full">
-          {/* Project Image */}
-          <div className="project-Img flex-col sm:flex-1 flex justify-center items-center">
-            <img
-              className="w-[90%] h-auto "
-              src="https://github.com/SiddharthP121/siddhartha-portfolio/blob/main/public/assets/protfolio.png?raw=true"
-              alt="Project Thumbnail"
-            />
+        {/* Project container: 1 */}
+
+        {projectsData.map((project) => (
+          <div
+            key={project.id}
+            className="my-10 sm:my-20 project-card flex flex-wrap justify-around items-start gap-8 w-full"
+          >
+            {/* Project Image */}
+            <div className="project-Img flex-col sm:flex-1 flex justify-center items-center">
+              <img
+                className="w-[90%] h-auto "
+                src={project.img}
+                alt="Project Thumbnail"
+              />
+            </div>
+
+            {/* Project Title and Description */}
+            <div className="project-about flex-1 flex flex-col justify-center items-start">
+              <h1 className="sub-heading mx-5 sm:mr-[5vw] text-2xl font-bold mb-4">
+                {project.name}
+              </h1>
+              <p className="description mx-5 sm:mr-[5vw] text-lg font-light text-[#4d5776]">
+                {project.description}
+              </p>
+              <button
+                onClick={()=> handleDetails(project.id)}
+                className="ui-btn self-end mr-[8vw] mt-[8vh]"
+              >
+                <span>Details</span>
+              </button>
+            </div>
           </div>
-
-          {/* Project Title and Description */}
-          <div className="project-about flex-1 flex flex-col justify-center items-start">
-            <h1 className="sub-heading mx-5 sm:mr-[5vw] text-2xl font-bold mb-4">
-             {"Portfolio -- Siddharth Potphode "}
-            </h1>
-            <p className="description mx-5 sm:mr-[5vw] text-lg font-light text-[#4d5776]">
-             This page is deployed on github pages and uses ReactJS (for frontend), NodeJS, ExpressJS (for backend) Tailwind CSS (for designs and responsiveness). The page depict the my overall progress, work and skills that I have learned. Various type of animated icons and buttons are used to make the good forntend and feel you modern. The code is updated on Git-hub and is open for all. For more info visit the project details page.
-            </p>
-            <button className="ui-btn self-end mr-[8vw] mt-[8vh]">
-              <span>Details</span>
-            </button>
-          </div>
-        </div>
-
-
-        
-
-        {/* Project container: 2 */}
-        <div className="project-card my-10 sm:my-20 flex flex-wrap justify-around items-start gap-8 w-full">
-          {/* Project Image */}
-          <div className="project-Img flex-col sm:flex-1 flex justify-center items-center">
-            <img
-              className="w-[90%] h-auto "
-              src="https://github.com/SiddharthP121/siddhartha-portfolio/blob/main/public/assets/We-con.png?raw=true"
-              alt="Project Thumbnail"
-            />
-          </div>
-
-          {/* Project Title and Description */}
-          <div className=" project-about flex-1 flex flex-col justify-center items-start">
-            <h1 className="sub-heading mx-5 sm:mr-[5vw] text-2xl font-bold mb-4">
-              {"We-Con -- Connect the people worldwide"}
-            </h1>
-            <p className="description mx-5 sm:mr-[5vw] text-lg font-light text-[#4d5776]">
-              We-Con is basically a social media plateform prototype. It is ReactJS based project with tailwind CSS implemented. The page has fully functional Login and Signup page which allows the user to register and make their account on it. The page is responsive and has a good UI. The page is built using ReactJS (for frontend), NodeJS, ExpressJS (for backend) Tailwind CSS (for designs and responsiveness). The page code is updated on GitHub. For more info visit the project details page.
-            </p>
-            <button className="ui-btn self-end mr-[8vw] mt-[8vh]">
-              <span>Details</span>
-            </button>
-          </div>
-        </div>
-
-
-        {/* Project container: 3 */}
-        
-        <div className="my-10 sm:my-20 project-card flex flex-wrap justify-around items-start gap-8 w-full">
-          {/* Project Image */}
-          <div className="project-Img flex-col sm:flex-1 flex justify-center items-center">
-            <img
-              className="w-[90%] h-auto "
-              src="https://github.com/SiddharthP121/siddhartha-portfolio/blob/main/public/assets/Passmanager.png?raw=true"
-              alt="Project Thumbnail"
-            />
-          </div>
-
-          {/* Project Title and Description */}
-          <div className="project-about flex-1 flex flex-col justify-center items-start">
-            <h1 className="sub-heading mx-5 sm:mr-[5vw] text-2xl font-bold mb-4">
-             {" Save-Pass -- Manage your passwords"}
-            </h1>
-            <p className="description mx-5 sm:mr-[5vw] text-lg font-light text-[#4d5776]">
-              Save-Pass is a password manager that allows users to securely
-              store and manage their passwords. It uses encryption to protect
-              sensitive information and provides a user-friendly interface for
-              easy access. The application is built using the MERN stack,
-              ensuring a robust and scalable solution for password management.
-              Passwords and the site data are stored in local storage. For more
-              info visit the project details page.
-            </p>
-            <button className="ui-btn self-end mr-[8vw] mt-[8vh]">
-              <span>Details</span>
-            </button>
-          </div>
-        </div>
-
-
-
-        {/* Project container: 4 */}
-        
-        <div className="my-10 sm:my-20 project-card flex flex-wrap justify-around items-start gap-8 w-full">
-          {/* Project Image */}
-          <div className="project-Img flex-col sm:flex-1 flex justify-center items-center">
-            <img
-              className="w-[90%] h-auto "
-              src="https://github.com/SiddharthP121/siddhartha-portfolio/blob/main/public/assets/netflix.png?raw=true"
-              alt="Project Thumbnail"
-            />
-          </div>
-
-          {/* Project Title and Description */}
-          <div className="project-about flex-1 flex flex-col justify-center items-start">
-            <h1 className="sub-heading mx-5 sm:mr-[5vw] text-2xl font-bold mb-4">
-             {"Netflix (Clone) -- Watch Movies Online"}
-            </h1>
-            <p className="description mx-5 sm:mr-[5vw] text-lg font-light text-[#4d5776]">
-             This is a single web page design to basically practice my HTML and CSS skills. It is a clone of the Netflix page. I've learned various and different type of basic concepts of CSS and HTML. The page is responsive and has a good UI. The page is built using HTML, CSS. The page code is updated on GitHub. For more info visit the project details page.
-            </p>
-            <button className="ui-btn self-end mr-[8vw] mt-[8vh]">
-              <span>Details</span>
-            </button>
-          </div>
-        </div>
+        ))}
       </div>
     </>
   );
